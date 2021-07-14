8caf31e12687dc595381b6943d88d173f50d9fbd
Paul Frazee <pfrazee@gmail.com>
2021-07-14 17:30:33 -0500

    Fixes to the indexer

--- index.js
+++ index.js
@@ -17,16 +17,18 @@ console.log = (...args) => {
   }
 }
 
-const _idxcache = {}
 const DATABASE = 'hyper'
 const ALL_DBS = []
 const NUM_DBS = Number(process.argv[2])
 const DB_FOR = docid => ALL_DBS[docid % NUM_DBS]
 const QUERIES = [
   'good',
+  'priest',
   'evening morning',
   'jacob isaac david',
-  'do unto others as you would',
+  'to be or not to be',
+  'comets importing',
+  'comets importing change of times and states',
   'how about a long query full of words which may or may not hit a bible verse?'
 ]
 if (DATABASE === 'hyper') await hyper.setup()
@@ -46,6 +48,9 @@ for (let i = 0; i < DOCS.length; i++) {
   await index(i, DOCS[i])
 }
 for (let db of ALL_DBS) {
+  for (let k in db._idxcache) {
+    await db.tx.put(k, db._idxcache[k])
+  }
   await db.tx.flush()
 }
 console.log({ts: true}, 'Done')
@@ -55,8 +60,10 @@ for (let query of QUERIES) {
     console.log(i+1, 'of', query)
     const results = await search(query)
     console.log({ts: true}, `Done: ${results.length} results`)
+    if (i === 0) console.log(results)
   }
 }
+process.exit(0)
 
 /*const tokens = toTokens(`
 In information retrieval, tf–idf, TF*IDF, or TFIDF, short for term frequency–inverse document frequency, is a numerical statistic that is intended to reflect how important a word is to a document in a collection or corpus.[1] It is often used as a weighting factor in searches of information retrieval, text mining, and user modeling. The tf–idf value increases proportionally to the number of times a word appears in the document and is offset by the number of documents in the corpus that contain the word, which helps to adjust for the fact that some words appear more frequently in general. tf–idf is one of the most popular term-weighting schemes today. A survey conducted in 2015 showed that 83% of text-based recommender systems in digital libraries use tf–idf.[2]
@@ -82,15 +89,17 @@ async function index (id, text) {
   
   // const tx = DB_FOR(id).batch()
   if (!db.tx) db.tx = db.batch()
+  if (!db._idxcache) db._idxcache = {}
   const tx = db.tx
 
   await tx.put(`docs:${id}`, {text})
   for (let token of toTokens(text)) {
-    // const item = await db.get(`idx:${token}:${id}`)
+    // const item = await db.get(`idx:${token}`)
     // const value = item?.value || {docIds: []}
-    const value = _idxcache[`idx:${token}:${id}`] || {docIds: []}
+    const value = db._idxcache[`idx:${token}`] || {docIds: []}
     value.docIds.push(id)
-    await tx.put(`idx:${token}`, value)
+    db._idxcache[`idx:${token}`] = value
+    // await tx.put(`idx:${token}`, value)
   }
   // await tx.flush()
 }
@@ -110,7 +119,7 @@ async function search (query, limit = 10) {
     const docIds = item.value ? item.value.docIds : item.docIds
     if (!docIds?.length) continue
     for (let docId of docIds) {
-      docIdHits[docId] = (docIdHits[docId] || 0) | 1
+      docIdHits[docId] = (docIdHits[docId] || 0) + 1
     }
   }
   const docIdsSorted = Object.keys(docIdHits).sort((a, b) => docIdHits[b] - docIdHits[a])
