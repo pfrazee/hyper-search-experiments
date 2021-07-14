34d3d278c2bb152feee9711645692a78ba854dfe
Paul Frazee <pfrazee@gmail.com>
2021-07-14 16:48:55 -0500

    Optimize indexing and fix the fake db

--- index.js
+++ index.js
@@ -17,15 +17,23 @@ console.log = (...args) => {
   }
 }
 
-const USE_FAKE = false
+const _idxcache = {}
+const DATABASE = 'hyper'
 const ALL_DBS = []
 const NUM_DBS = Number(process.argv[2])
 const DB_FOR = docid => ALL_DBS[docid % NUM_DBS]
-if (!USE_FAKE) await hyper.setup()
+const QUERIES = [
+  'good',
+  'evening morning',
+  'jacob isaac david',
+  'do unto others as you would',
+  'how about a long query full of words which may or may not hit a bible verse?'
+]
+if (DATABASE === 'hyper') await hyper.setup()
 for (let i = 0; i < NUM_DBS; i++) {
-  if (USE_FAKE) {
+  if (DATABASE === 'fake') {
     ALL_DBS.push(new FakeDB())
-  } else {
+  } else if (DATABASE === 'hyper') {
     ALL_DBS.push(hyper.create())
   }
 }
@@ -37,17 +45,18 @@ console.log('Indexing (this may take multiple minutes)...')
 for (let i = 0; i < DOCS.length; i++) {
   await index(i, DOCS[i])
 }
-// for (let db of ALL_DBS) {
-//   await db.tx.flush()
-// }
+for (let db of ALL_DBS) {
+  await db.tx.flush()
+}
 console.log({ts: true}, 'Done')
 
-console.log('Executing query...')
-const query = process.argv.slice(3)
-const results = await search(query)
-console.log({ts: true}, 'Done')
-console.log('Search results for:', query.join(' '))
-console.log(results)
+for (let query of QUERIES) {
+  for (let i = 0; i < 5; i++) {
+    console.log(i+1, 'of', query)
+    const results = await search(query)
+    console.log({ts: true}, `Done: ${results.length} results`)
+  }
+}
 
 /*const tokens = toTokens(`
 In information retrieval, tf–idf, TF*IDF, or TFIDF, short for term frequency–inverse document frequency, is a numerical statistic that is intended to reflect how important a word is to a document in a collection or corpus.[1] It is often used as a weighting factor in searches of information retrieval, text mining, and user modeling. The tf–idf value increases proportionally to the number of times a word appears in the document and is offset by the number of documents in the corpus that contain the word, which helps to adjust for the fact that some words appear more frequently in general. tf–idf is one of the most popular term-weighting schemes today. A survey conducted in 2015 showed that 83% of text-based recommender systems in digital libraries use tf–idf.[2]
@@ -71,18 +80,19 @@ function toTokens (str) {
 async function index (id, text) {
   const db = DB_FOR(id)
   
-  const tx = DB_FOR(id).batch()
-  // if (!db.tx) db.tx = db.batch()
-  // const tx = db.tx
+  // const tx = DB_FOR(id).batch()
+  if (!db.tx) db.tx = db.batch()
+  const tx = db.tx
 
   await tx.put(`docs:${id}`, {text})
   for (let token of toTokens(text)) {
-    const item = await db.get(`idx:${token}:${id}`)
-    const value = item?.value || {docIds: []}
+    // const item = await db.get(`idx:${token}:${id}`)
+    // const value = item?.value || {docIds: []}
+    const value = _idxcache[`idx:${token}:${id}`] || {docIds: []}
     value.docIds.push(id)
     await tx.put(`idx:${token}`, value)
   }
-  await tx.flush()
+  // await tx.flush()
 }
 
 async function search (query, limit = 10) {
@@ -96,8 +106,10 @@ async function search (query, limit = 10) {
   const getResults = await Promise.all(getsPromises)
   const docIdHits = {}
   for (let item of getResults) {
-    if (!item?.value?.docIds?.length) continue
-    for (let docId of item.value.docIds) {
+    if (!item) continue
+    const docIds = item.value ? item.value.docIds : item.docIds
+    if (!docIds?.length) continue
+    for (let docId of docIds) {
       docIdHits[docId] = (docIdHits[docId] || 0) | 1
     }
   }
